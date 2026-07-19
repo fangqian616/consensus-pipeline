65428df591e7d45dba5c8a29e8179a9c2938dff2
fangqian616 <1362063591@qq.com>
2026-07-19 14:50:09 +0800

    fix: remove 3 ghost academic keys not in ACADEMIC_DEPARTMENT_TEMPLATE
    cross_validation, theme_clustering, report_writing don't exist in the template.
    All three academic_keys sets now identical: 11 keys.

--- debate_engine.py
+++ debate_engine.py
@@ -769,7 +769,7 @@ def apply_config(config: dict):
     _academic_keys = {"literature_search", "methodology_review", "report_integration",
                       "programming", "tutorial", "metadata_inspector", "citation_network",
                       "data_validation", "counter_evidence", "topic_clustering",
-                      "cross_validation", "theme_clustering", "visualization", "report_writing"}
+                      "visualization"}
     _dept_keys = set(config.get("departments", {}).keys())
     if any(k in _dept_keys for k in _academic_keys):
         ANIME_VISUAL_DIRECTIVE.clear()
